183013b322b827e985033db9244777d589ba6349
adelinel <adeline.latruwe@gmail.com>
2021-02-18 13:07:45 -0800

    setting up cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const { CLIENT_ORIGIN } = require("./config");
 
 const app = express();
 
@@ -11,7 +12,11 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
