83ca30754b0c4f8f93313390020c65e505d9ac10
adelinel <adeline.latruwe@gmail.com>
2021-02-19 11:08:58 -0800

    connected database to server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
-const app = require("./app");
+require("dotenv").config();
 
-const { PORT } = require("./config");
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DB_URL } = require("./config");
+
+const db = knex({
+  client: "pg",
+  connection: DB_URL,
+});
+console.log("knex and driver installed correctly");
+
+db("users")
+  .select("*")
+  .then((results) => {
+    console.log(results);
+  });
+
+app.set("db", db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
