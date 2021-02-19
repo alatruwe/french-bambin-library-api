15c268de0c68ecbf6cf8f61cf254f29ff5f4165f
adelinel <adeline.latruwe@gmail.com>
2021-02-19 11:16:25 -0800

    cleaned testing code

--- src/server.js
+++ src/server.js
@@ -8,13 +8,6 @@ const db = knex({
   client: "pg",
   connection: DB_URL,
 });
-console.log("knex and driver installed correctly");
-
-db("users")
-  .select("*")
-  .then((results) => {
-    console.log(results);
-  });
 
 app.set("db", db);
 
