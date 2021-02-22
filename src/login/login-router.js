f3f7f789943bb2fe88fb1ad117491f6aa2b2e811
adelinel <adeline.latruwe@gmail.com>
2021-02-22 11:17:20 -0800

    added refresh token endpoint

--- src/login/login-router.js
+++ src/login/login-router.js
@@ -39,4 +39,12 @@ loginRouter.post("/", jsonBodyParser, (req, res, next) => {
     .catch(next);
 });
 
+loginRouter.post("/refresh", requireAuth, (req, res) => {
+  const sub = req.user.email;
+  const payload = { user_id: req.user.id };
+  res.send({
+    authToken: AuthService.createJwt(sub, payload),
+  });
+});
+
 module.exports = loginRouter;
