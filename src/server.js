33a56b0e02ffa248f5fbb59ee35dc9459e4ae310
adelinel <adeline.latruwe@gmail.com>
2021-02-26 18:19:23 -0800

    database deploy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,10 @@ const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL,
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
 });
 
 app.set("db", db);
