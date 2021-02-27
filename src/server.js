f16c2813b6885680f0740dc90fabbd068baa113d
adelinel <adeline.latruwe@gmail.com>
2021-02-26 20:23:41 -0800

    database update

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const { PORT, DATABASE_URL } = require("./config");
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: DATABASE_URL,
-    ssl: true,
+    connectionString:
+      "dbname=dbdg3vi0eik7la host=ec2-34-230-167-186.compute-1.amazonaws.com port=5432 user=lgwuruogcfvjce password=02ca089f1963c09e6d9da16339a7f4d2c2c963dc5a45668a5edefa4694274d15 sslmode=require",
   },
 });
 
